36fa30143d1d98585f00549a58c12a7510d8598d
3nigma-tik <cole.blackwood@icloud.com>
2021-11-02 13:23:52 -0700

    added nav buttons for 'next' and 'previous' books

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { isElementOfType } from "react-dom/test-utils";
 import './App.css'
 import TitleBar from "./TitleBar/TitleBar";
 
@@ -11,9 +12,31 @@ class App extends Component {
             {title: "The First and Last Freedom", author: "Jiddu Krishnamurit"} 
         ];
         this.state = {
-            bookNumber: 1
+            bookNumber: 2
         };
             
+    }
+
+    goToNextBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber++;
+        if(tempBookNumber === this.books.length){
+            tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if(tempBookNumber < 0) {
+            tempBookNumber = this.books.length - 1;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
     }
 
     render(){
@@ -21,16 +44,18 @@ class App extends Component {
             <div className="container-fluid">
                 <TitleBar />
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         {/*Button herer to move to the previous book viewed*/}
+                        <button onClick={this.goToPreviousBook}>Previous Book</button>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-6">
                         {/*display book with cover here*/}
                         <h1>{this.books[this.state.bookNumber].title}</h1>
                         <h4>{this.books[this.state.bookNumber].author}</h4>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-3">
                         {/*Button to move to the next book here*/}
+                        <button onClick={this.goToNextBook}>Next Book</button>
                     </div>
                 </div>
             </div>
